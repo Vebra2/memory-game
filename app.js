61a8f4eb4332cbc23850fa0d68feb4695c84a35e
wilfried_nzekeng <wilfried.nzekeng@gmail.com>
2022-06-18 13:23:53 +0200

    just proceeding

--- app.js
+++ app.js
@@ -52,13 +52,39 @@ const cardArray = [
 
 cardArray.sort(() => 0.5 - Math.random());
 
-const gridDisplay = document.querySelector(".grid");
+const gridDisplay = document.querySelector("#grid");
+const cardChoosen = [];
+const cardChoosenIds = [];
 
 function createBoard() {
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < cardArray.length; i++) {
     const card = document.createElement("img");
     card.setAttribute("src", "images/blank.png");
-    console.log(card, i);
+    card.setAttribute("data-id", i);
+    card.addEventListener("click", flipCard);
+    gridDisplay.appendChild(card);
   }
 }
 createBoard();
+
+function checkMatch() {
+  const cards = document.querySelectorAll("#grid img");
+  console.log(cards);
+  console.log("check for match");
+  if (cardChoosen[0] == cardChoosen[1]) {
+    alert("you found a match");
+    cards[cardChoosenIds[0]].setAttribute("src", "images/white.png");
+    cards[cardChoosenIds[1]].setAttribute("src", "images/white.png");
+  }
+}
+function flipCard() {
+  const cardId = this.getAttribute("data-id");
+  cardChoosen.push(cardArray[cardId].name);
+  this.setAttribute("src", cardArray[cardId].img);
+  cardChoosenIds.push(cardId);
+  console.log(cardChoosenIds);
+  console.log(cardChoosen);
+  if (cardChoosen.length === 2) {
+    setTimeout(checkMatch, 500);
+  }
+}
